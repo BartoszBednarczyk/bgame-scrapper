7df9e6b15837247ab262e951a0f08a5beaeb1015
Bartosz Bednarczyk <bednarczykbartosz@outlook.com>
2022-05-28 16:15:51 +0200

    Update cors

--- app.js
+++ app.js
@@ -12,29 +12,6 @@ const AXIOS_OPTIONS = {
 
 var app = express();
 app.use(cors());
-var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-  // intercept OPTIONS method
-  if ('OPTIONS' == req.method) {
-    res.send(200);
-  }
-  else {
-    next();
-  }
-};
-
-app.configure(function () {
-  app.use(allowCrossDomain);
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  
-  app.use(express.static(path.join(application_root, "public")));
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
 
 app.listen(process.env.PORT || 3000, () => {
  console.log("Server running on port 3000");
